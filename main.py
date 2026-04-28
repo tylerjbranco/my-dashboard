15123b7e33e0e05cdaab56325315835cdfd23e09
tylerjbranco <tylerjbranco@gmail.com>
2026-04-28 09:50:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1461,9 +1461,9 @@ def render_game_card(game):
                     trailer_wins = min(h, a)
                     # Figure out which team leads
                     if h > a:
-                        leader_name = competition["competitors"][0]["team"].get("location", home_team)
+                        leader_name = home_team
                     else:
-                        leader_name = competition["competitors"][1]["team"].get("location", away_team)
+                        leader_name = away_team
                     if leader_wins == 4:
                         series_summary = f"{leader_name} win {leader_wins}-{trailer_wins}"
                     else:
